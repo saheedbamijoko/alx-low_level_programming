ee1841f02fdcab7b2a5824c901af85f9a73174c7
saheed bamijoko <saheedbamijoko3@gmail.com>
2021-02-04 15:32:51 +0000

    Variables

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -14,8 +14,8 @@ for (tens = 0; tens <= 9; tens++)
 {
 for (ones = 0; ones <= 9; ones++)
 {
-putchar(tens + '0')
-putchar(ones + '0')
+putchar(tens + '0');
+putchar(ones + '0');
 if (tens != 9 || ones != 9)
 {
 putchar(',');
