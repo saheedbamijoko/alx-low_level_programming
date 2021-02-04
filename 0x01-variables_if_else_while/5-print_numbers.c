9e29b255fb9d9d434769242f01c89f6e883ebfed
saheed bamijoko <saheedbamijoko3@gmail.com>
2021-02-04 13:51:59 +0000

    C-variables,if,else,while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdi.h>
+
+/**
+ * main - Print all sinle digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+printf("%i", digit);
+
+return (0);
+}
