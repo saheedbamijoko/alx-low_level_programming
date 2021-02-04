9a1b9d743219174ee64d58d057d912a9c7024b77
saheed bamijoko <saheedbamijoko3@gmail.com>
2021-02-04 13:07:28 +0000

    C-Variables,if,else,while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++) /* Print lowercase letter a-z */
+{
+putchar('ch');
+}
+for (ch = 'A'; ch <= 'Z'; ch++) /* Print Upper letters A-Z */
+{
+putchar('ch');
+}
+putchar('\n');
+
+return (0);
+}
