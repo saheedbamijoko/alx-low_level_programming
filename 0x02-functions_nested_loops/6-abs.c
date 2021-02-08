769753de4d3158cae053d0d8b83c1e9827cbad7b
saheed bamijoko <saheedbamijoko3@gmail.com>
2021-02-08 16:59:22 +0000

    C-functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton"
+
+/**
+ * main - function that conpute absolute value of an integer
+ * @n: number as integer
+ * Return: absolute value
+ */
+
+int int_abs(int n);
+int main(void)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (0);
+}
