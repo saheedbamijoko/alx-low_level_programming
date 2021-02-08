c770c3d07fa1c7fb45b7cf880e259aa6cd642a60
saheed bamijoko <saheedbamijoko3@gmail.com>
2021-02-08 15:59:33 +0000

    C-functions,nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - check for lowercase character
+ * @c: character to check
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+
+int _islower(int c);
+int main(void)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
