f530b1f3495652bcb5a0edbbb0bb27acea536d91
saheed bamijoko <saheedbamijoko3@gmail.com>
2021-02-08 15:24:06 +0000

    C-functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - print alphabet in lower case
+ * Return: void.
+ */
+
+void print_alphabet(void);
+int main(void)
+
+{
+char print_alphabet;
+
+for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
+{
+_putchar(print_alphabet);
+}
+_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - print 10times the alphabet in lowercase
+ * Return: void.
+ */
+
+void print_alphabet_x10(void);
+int main(void)
+{
+char letter;
+int counter;
+
+for (counter = 1; counter <= 10; counter++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar ('\n');
+}
+}
