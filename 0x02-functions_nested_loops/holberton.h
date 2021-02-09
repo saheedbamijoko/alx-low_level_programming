b2aa973a57987d133ab175a151f09ef23b569533
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-09 07:01:45 +0100

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -13,11 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-void pr_digit(int charcode, int fmt);
-void pr_with_fmt(int charcode, int fmt);
-void pr_3_digit(int charcode, int fmt);
-void print_times_table(int n);
-void print_spaces(int n);
-void print_zero(void);
 
 #endif
