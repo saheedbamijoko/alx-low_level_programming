5f7bf6a98a198d3058632026c91ace116df02e22
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-09 10:24:56 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - print sign of a number
+ * print_sign - print sign of a number
  * n: number to check
  * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
