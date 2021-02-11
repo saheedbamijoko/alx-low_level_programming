1c35d45fa40ac2e28f0db1188bb099aacdd0c34a
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-11 13:50:25 +0100

    Update holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -9,6 +9,8 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(in n);
-
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif
