eb0295ed184a9641773d504a7708a285840586fe
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-11 14:34:55 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,9 +7,13 @@
 
 void print_line(int n)
 {
-int times = n;
-
-for (times = n; times > 0; times--)
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
 _putchar('_');
-_putchar'\n');
+}
+}
+_putchar('\n');
 }
