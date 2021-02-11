7b316ea57ee8611b108331004589051f3aaacc2c
[saheedbamijoko] <[saheedbamijoko3@gmail.com]>
2021-02-11 11:04:01 +0000

    working with functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @: second integer
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+int x;
+
+x = (a * b);
+}
