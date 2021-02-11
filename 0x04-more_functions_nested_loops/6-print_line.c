253f46b63fca97c7909ceb6e9c1d8c9fccd8cb1c
[saheedbamijoko] <[saheedbamijoko3@gmail.com]>
2021-02-11 12:39:29 +0000

    functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_line - write a function that draws a straight line in the terminal
+ * @n: number of characters to draw
+ */
+
+void print_line(int n)
+{
+char c;
+
+for (c = 0; c < n; c++)
+_putchar('_');
+_putchar'\n');
+}
