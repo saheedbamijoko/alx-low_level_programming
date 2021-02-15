97097f9b4048d5196518542696606da65c2af915
[saheedbamijoko] <[saheedbamijoko3@gmail.com]>
2021-02-15 11:23:19 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * void reset_to_98(int *n) - funtion that takes a pointer to an int
+ * @n: number
+ * Return: Always 0 (Success)
+ */
+
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x05. Pointers, arrays and strings.
+
+This directory contains projects of pointers, arrays and strings.
