47a54e8c1b570beba657e37cd4edcc19322b959c
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-16 09:50:11 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,16 @@ void print_rev(char *s)
 {
 int len = 0;
 int i;
-
+/* iterate to find length of string and point to last character */
 while (*s != '\0')
 {
 len++;
 ++s;
 }
+
+/* go back to character before null character */ 
 s--;
+/* print string reversed */
 for (i = len; i > 0; i--)
 {
 _putchar(*s);
