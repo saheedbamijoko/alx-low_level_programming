b1f2705f11b818ab9f5adf8318a164f7e0742b82
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-22 22:10:18 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
-#define NULL 0
+#include <stddef.h>
+
 
 /**
- * _strchr - locate 1st occurrence of char in string and returns pointer there
- * @s: string to search
- * @c: target characer
- * Return: pointer to that character in string
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: a pointer to a character aka the character found
  */
-
 char *_strchr(char *s, char c)
 {
-int i = 0;
+int i;
 
-while (s[i] != '\0' && s[i] != c) /* find match */
-i++;
-
-if (s[i] == c) /* if match, assign to address */
+for (i = 0; s[i] != '\0' ; i++)
+{
+if (s[i] == c)
 return (&s[i]);
-	
-else
-return (NULL);
+}
+return (0);
 }
