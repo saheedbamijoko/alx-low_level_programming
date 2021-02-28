e62a9d1b254b2adae51deb12c25baec21b81a570
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-28 22:43:06 +0100

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * main - prints program name
+  * @argc: number of arguments
+  * @argv: array of arguments
+  * Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
