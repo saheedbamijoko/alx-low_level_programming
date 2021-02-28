91c90e757c82e141d9fe2532b0efa83942e230ac
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-02-28 22:49:30 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,25 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
+
 /**
-  * main - multiplies two numbers
-  * @argc: number of arguments
-  * @argv: array of arguments
-  * Return: 0 if  input correct, 1 otherwise
-  */
+ * main - print product of argument numbers
+ * @argc: argument counter
+ * @argv: numbers to multiply
+ * Return: 0 on success, 1 if two arguments not given
+ */
+
 int main(int argc, char *argv[])
 {
-int first, second;
 
-if (argc == 3)
-{
-first = atoi(argv[1]);
-second = atoi(argv[2]);
-printf("%d\n", first * second);
-return (0);
-}
-else
+/* validate input */
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+
+/* mulitply two arguments passed via cmd line */
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
 }
