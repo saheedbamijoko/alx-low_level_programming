0066020ffd2bf51cb7397ee7ea8c23a13a6f7a16
Saheed Bamijoko <77827010+saheedbamijoko@users.noreply.github.com>
2021-03-20 09:41:51 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,52 +1,69 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
- * _strlen - find string length
- * @str: string
- * Return: length
+ * _strlen - gets length of the string
+ * @s: string
+ * Return: length of the string
  */
-int _strlen(const char *str)
+int _strlen(const char *s)
 {
-int len;
+int i;
 
-for (len = 0; str[len] != '\0'; len++)
+for (i = 0; s[i]; i++)
 ;
-return (len);
+return (i);
 }
-
 /**
- * add_node_end - add node to end of linked list
- * @head: linked list
- * @str: data for new node
- * Return: address of new element, or NULL if failed
+ * _strdup - recreation of string duplicate function
+ * @src: source of string to duplicate
+ * Return: pointer to malloc'd space with copied string
+ */
+void *_strdup(const char *src)
+{
+int len, i;
+char *dest;
+
+len = _strlen(src);
+dest = malloc((len + 1) * sizeof(char));
+if (dest == NULL)
+return (NULL);
+for (i = 0; src[i]; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
+}
+/**
+ * add_node_end - add new nodes to the end of the list
+ * @head: current place in the list
+ * @str: string to add to the head
+ * Return: pointer to current position in list
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *tmp; /* create new node */
+list_t *new, *current;
+char *dupstr;
 
-if (str == NULL) /* validate input */
+if (str == NULL)
 return (NULL);
-if (strdup(str) == NULL) /* check if malloc errored */
+dupstr = _strdup(str);
+if (dupstr == NULL)
 return (NULL);
-
-new_node = malloc(sizeof(list_t)); /* malloc for new node */
-if (new_node == NULL)
+new = malloc(sizeof(list_t));
+if (new == NULL)
 return (NULL);
-
-new_node->str = strdup(str); /* set node values */
-new_node->len = _strlen(str);
-new_node->next = NULL;
-
-if (*head == NULL) /* if no list nodes, set new_node to beginning */
-*head = new_node;
-else
+new->str = dupstr;
+new->len = _strlen(str);
+new->next = NULL;
+if (*head == NULL)
 {
-tmp = *head;
-
-while (tmp->next != NULL)
-tmp = tmp->next;
-tmp->next = new_node;
+*head = new;
+return (*head);
 }
-
-return (new_node);
+current = *head;
+while (current->next != NULL)
+current = current->next;
+current->next = new;
+return (*head);
 }
